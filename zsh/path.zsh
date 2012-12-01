21b16099e565586d504c638e2ed54c9eb9299db3
albertogg <albertogrespan@gmail.com>
2012-12-01 19:01:34 -0430

    rbenv, subl and oh-my-zsh update

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,7 +1,15 @@
 # prepend to PATH.
+
+# search for rbenv.
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# add python path for hombrew version.
+export PATH="/usr/local/share/python:$PATH"
 # add coreutils.
 # add homebrew installed packages.
-export PATH="/usr/local/share/python:$PATH"
+# last export will be the first in path!
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH"
+
+
 # remove duplicates from PATH.
 typeset -U PATH
