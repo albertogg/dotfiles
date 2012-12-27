568551f986ccaf33454ed638976ccc6110e2a969
albertogg <albertogrespan@gmail.com>
2012-12-27 13:50:50 -0430

    vimrc and path shit

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,18 +1,24 @@
+##################
 # prepend to PATH.
 
-# export heroku postgres.app to PATH if exists.
-if [[ -e /Applications/Postgres.app ]]; then export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"; fi
+##
+# if exists, export heroku postgres.app to PATH.
+if [[ -e "/Applications/Postgres.app" ]]; then export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"; fi
 
+##
 # search for rbenv.
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-# add python path for hombrew version.
-export PATH="/usr/local/share/python:$PATH"
+##
+# if exists, add path for homebrew installed python.
+if [[ -e "/usr/local/share/python" ]]; then export PATH="/usr/local/share/python:$PATH"; fi
+
+##
 # add coreutils.
 # add homebrew installed packages.
 # last export will be the first in path!
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:$PATH"
 
-
+##
 # remove duplicates from PATH.
 typeset -U PATH
