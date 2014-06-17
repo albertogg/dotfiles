539f1e0287576d28e543ca260788ee49282ccea6
Alberto Grespan <albertogrespan@gmail.com>
2014-06-16 23:32:05 -0430

    Add the rest of thoughtbot awesome key bindings

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -45,3 +45,6 @@ bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
 bindkey "^R" history-incremental-search-backward
 bindkey "^P" history-search-backward
+bindkey "^Y" accept-and-hold
+bindkey "^N" insert-last-word
+bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
