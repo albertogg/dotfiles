ee38191e37bb03a3ce915b66206eab1424b658f1
Alberto Grespan <albertogrespan@gmail.com>
2014-08-10 16:22:20 -0430

    zsh history options

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,16 +6,20 @@ colors
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
 
 # History settings
-setopt hist_ignore_all_dups
-setopt inc_append_history
-setopt hist_reduce_blanks
-setopt extended_history
-setopt share_history
 if [ -z $HISTFILE ]; then
     HISTFILE=$HOME/.zsh_history
 fi
 HISTSIZE=10000
 SAVEHIST=10000
+
+setopt bang_hist
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history
 
 # Awesome cd movements
 setopt autocd autopushd pushdminus pushdsilent pushdtohome cdablevars
