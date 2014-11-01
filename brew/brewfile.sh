23f2767436bd41d506e15551556f60f6c34dee1f
Alberto Grespan <albertogrespan@gmail.com>
2014-11-01 17:15:02 -0430

    Update the homebrew installation script

---
+++ brew/brewfile.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env sh
+
+install_brews() {
+
+    # taps
+    brew tap nviennot/tmate
+
+    # system formulae
+    brew install zsh
+    brew install ack
+    brew install coreutils
+    brew install git
+    brew install hg
+    brew install tree
+    brew install wget
+    brew install ssh-copy-id
+    brew install ctags
+    brew install reattach-to-user-namespace
+
+    # homebrew cask formulae
+    brew install caskroom/cask/brew-cask
+
+    # development formulae
+    brew install rbenv
+    brew install ruby-build
+    brew install rbenv-gem-rehash
+    brew install rbenv-vars
+    brew install rbenv-default-gems
+    brew install tmux
+    brew install tmate
+    brew install go
+
+    # clean up
+    brew cleanup
+}
---
+++ brew/caskfile.sh
@@ -0,0 +1,16 @@
+install_casks() {
+
+    # Quick Look plugins
+    brew cask install qlcolorcode
+    brew cask install qlstephen
+    brew cask install qlmarkdown
+    brew cask install quicklook-json
+    brew cask install quicklook-csv
+    brew cask install webp-quicklook
+
+    # applications
+    brew cask install google-chrome
+    brew cask install macvim
+    brew cask install iterm2-nightly
+    brew cask install gitx-rowanj
+}
