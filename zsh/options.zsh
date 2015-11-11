e8e46acf16e7a96ce2e9a1ec953d9800d81f0ac8
Alberto Grespan <albertogrespan@gmail.com>
2015-11-11 13:35:36 -0500

    Rename and split the config.zsh file

---
+++ zsh/options.zsh
@@ -0,0 +1,21 @@
+# History
+setopt append_history
+setopt inc_append_history
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_verify
+setopt share_history
+setopt extended_history
+setopt bang_hist
+
+# Completion
+setopt always_to_end
+setopt auto_menu         # show completion menu on succesive tab press
+setopt complete_in_word  # cursor is set to the end of the word if completion is started
+unsetopt menu_complete   # do not autoselect the first completion entry
+
+# Directories
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushd_minus
+setopt extendedglob
