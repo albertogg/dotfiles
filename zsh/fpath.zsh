d5fcc90cbaebaccaf8686a12c8c4dddef3612555
Alberto Grespan <albertogrespan@gmail.com>
2015-11-11 13:35:49 -0500

    Easier to read fpath

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,2 @@
-#add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($ZSH/*; $(brew --prefix)/share/zsh/site-functions) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+# load our own completion functions
+fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
