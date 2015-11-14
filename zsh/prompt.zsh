b5698bfdebc10ad32a72b96c8fa447bd0450c791
Alberto Grespan <albertogrespan@gmail.com>
2015-11-14 12:48:44 -0430

    Prettier parse dirty

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,20 +1,17 @@
 git_prompt_info() {
   current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
   if [[ -n $current_branch ]]; then
-    parse_git_dirty
+    echo " %B%F{241}$current_branch%f%b$(parse_git_dirty)%{$reset_color%}"
   fi
 }
 
 parse_git_dirty() {
-  if [[ $(git status --porcelain 2> /dev/null) != "" ]]; then
-    set_git_prompt_color 214 # orange color for dirty branch
-  else
-    set_git_prompt_color 241 # gray color for clean branch
+  if [[ $(command git status --porcelain --ignore-submodules -unormal) != "" ]]; then
+    echo " %B%F{220}*%f%b"
   fi
 }
 
 set_git_prompt_color() {
-  echo " %B%F{$1}$current_branch%f%b%{$reset_color%}"
 }
 
 setopt promptsubst
