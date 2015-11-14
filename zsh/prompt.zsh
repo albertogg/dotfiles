c0c975e971bec2d0022f804290a52e0d8040c031
Alberto Grespan <albertogrespan@gmail.com>
2015-11-14 12:25:02 -0430

    Parse git dirty function

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,21 @@
 git_prompt_info() {
   current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
   if [[ -n $current_branch ]]; then
-    echo " %B%F{241}$current_branch%f%b%{$reset_color%}"
+    parse_git_dirty
   fi
 }
+
+parse_git_dirty() {
+  if [[ $(git status --porcelain 2> /dev/null) != "" ]]; then
+    set_git_prompt_color 214 # orange color for dirty branch
+  else
+    set_git_prompt_color 241 # gray color for clean branch
+  fi
+}
+
+set_git_prompt_color() {
+  echo " %B%F{$1}$current_branch%f%b%{$reset_color%}"
+}
+
 setopt promptsubst
 export PS1='%B%F{044}%c%f%b%{$reset_color%}$(git_prompt_info) %# '
