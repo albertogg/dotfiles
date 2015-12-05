4623c467eb2a950ef08b493b98b8789f0dd051e4
Alberto Grespan <albertogrespan@gmail.com>
2015-12-05 12:03:04 -0430

    Use control-B in tmux

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -5,6 +5,7 @@ bindkey jj vi-cmd-mode
 
 # handy keybindings
 bindkey "^A" beginning-of-line
+bindkey "^B" beginning-of-line # to use in tmux
 bindkey "^E" end-of-line
 bindkey "^R" history-incremental-search-backward
 bindkey "^P" history-search-backward
