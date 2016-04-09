90faa0e9fc4a136ff37bca719227011b16e1bf7b
Alberto Grespan <albertogrespan@gmail.com>
2016-04-09 11:40:49 -0430

    Move docker-machine alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,3 +1,7 @@
 if command -v gls >/dev/null; then
   alias ls="gls --color"
 fi
+
+if command -v docker-machine >/dev/null; then
+  alias dm='docker-machine'
+fi
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,1 @@
 alias reload!=". ~/.zshrc"
-alias dm='docker-machine'
