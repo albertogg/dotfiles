6794559ec3078d4418fe22dd4b13f1480b665e14
Alberto Grespan <alberto@albertogrespan.com>
2016-10-09 14:05:20 -0400

    Substitute ctags for universal-ctags
    This is the formula for a maintained version of ctags.

--- brew/brewfile.sh
+++ brew/brewfile.sh
@@ -1,11 +1,14 @@
 install_brews() {
+    # taps
+    brew tap universal-ctags/universal-ctags
+
     # system formulae
     brew install openssl
     brew install zsh
     brew install tmux
     brew install tmate
     brew install coreutils
-    brew install ctags
+    brew install --HEAD universal-ctags
     brew install git
     brew install vim --with-lua --with-override-system-vi
 
