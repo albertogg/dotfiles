441a51e82e778d8838ae0794e284ea238a7ba662
Alberto Grespan <alberto@albertogrespan.com>
2019-06-02 15:09:54 -0400

    Move the history options to the history file

--- zsh/.zsh/options.zsh
+++ zsh/.zsh/options.zsh
@@ -1,13 +1,3 @@
-# History
-setopt append_history
-setopt inc_append_history
-setopt hist_ignore_dups
-setopt hist_ignore_space
-setopt hist_verify
-setopt share_history
-setopt extended_history
-setopt bang_hist
-
 # Completion
 setopt always_to_end
 setopt auto_menu         # show completion menu on succesive tab press
