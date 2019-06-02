25222922bc737d167737adcc7069fdd63d7ed834
Alberto Grespan <alberto@albertogrespan.com>
2019-06-02 15:11:01 -0400

    Update the completions file
    Update the completions file and include the fpath, and the options in it

--- zsh/.zsh/completion.zsh
+++ zsh/.zsh/completion.zsh
@@ -1,18 +1,33 @@
 zmodload -i zsh/complist
 
-## case-insensitive (all),partial-word and then substring completion
-if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
-  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-  unset CASE_SENSITIVE
-else
-  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-fi
-
-zstyle ':completion:*' list-colors ''
+# Completion
+setopt always_to_end
+setopt auto_menu         # show completion menu on succesive tab press
+setopt complete_in_word  # cursor is set to the end of the word if completion is started
+unsetopt menu_complete   # do not autoselect the first completion entry
 
 # should this be in keybindings?
 bindkey -M menuselect '^o' accept-and-infer-next-history
-
 zstyle ':completion:*:*:*:*:*' menu select
+
+# load our own completion functions
+fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
+autoload -Uz compinit
+compinit
+
+# case insensitive (all), partial-word and substring completion
+if [[ "$CASE_SENSITIVE" = true ]]; then
+  zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
+else
+  if [[ "$HYPHEN_INSENSITIVE" = true ]]; then
+    zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
+  else
+    zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+  fi
+fi
+unset CASE_SENSITIVE HYPHEN_INSENSITIVE
+
+zstyle ':completion:*' list-colors ''
+
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
--- zsh/.zsh/options.zsh
+++
@@ -1,6 +0,0 @@
-# Completion
-setopt always_to_end
-setopt auto_menu         # show completion menu on succesive tab press
-setopt complete_in_word  # cursor is set to the end of the word if completion is started
-unsetopt menu_complete   # do not autoselect the first completion entry
-
