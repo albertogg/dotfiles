feeddc9e7e2841a420bcdde4dab86b2cf0356ee5
Alberto Grespan <alberto@albertogrespan.com>
2019-06-02 15:38:58 -0400

    Create a brew.sh and remove the brew and cask file

---
+++ brew.sh
@@ -0,0 +1,40 @@
+# System formulae
+brew install openssl
+brew install zsh
+brew install tmux
+brew install tmate
+brew install tree
+brew install coreutils
+brew install git
+brew install ripgrep
+brew install vim
+brew install reattach-to-user-namespace
+
+# Development formulae
+brew install rbenv
+brew install rbenv-vars
+brew install rbenv-default-gems
+brew install ruby-build
+brew install go
+brew install jenv
+
+# Yubikey
+brew install ykclient
+brew install ykpers
+brew install yubico-piv-tool
+brew install pam_yubico
+
+# Taps
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+# Applications
+brew cask install appcleaner
+brew cask install dropbox
+brew cask install transmission
+brew cask install gitup
+brew cask install tunnelblick
+brew cask install nordvpn
+brew cask install keybase
+brew cask install yubico-yubikey-piv-manager
+brew cask install logitech-options
