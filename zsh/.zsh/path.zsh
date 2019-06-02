a0a719cb3c2825eefa04fba3e33cf9e9101daadc
Alberto Grespan <alberto@albertogrespan.com>
2019-06-02 15:39:29 -0400

    Move the System files to the zsh directory

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -1,8 +1,8 @@
-# add coreutils.
-# add coreutils manuals.
-# add homebrew installed packages.
+# Add coreutils.
+# Add coreutils manuals.
+# Add homebrew installed packages.
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
-# remove duplicates from PATH.
+# Remove duplicates from PATH.
 typeset -U PATH
