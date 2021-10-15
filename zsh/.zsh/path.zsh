6f7f726d8a9d424f13099794b4a6fab224cfb691
Alberto Grespan <alberto@albertogrespan.com>
2021-10-14 22:43:19 -0400

    Fix the PATH on M1 chip

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -1,8 +1,14 @@
 # Add coreutils.
 # Add coreutils manuals.
 # Add homebrew installed packages.
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/local/sbin:$PATH"
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+# Homebrew directory for Apple M1 ARM chip
+if [ -f /opt/homebrew/bin/brew ]; then
+  export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+else
+  export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/local/sbin:$PATH"
+  export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+fi
 
 # Remove duplicates from PATH.
 typeset -U PATH
