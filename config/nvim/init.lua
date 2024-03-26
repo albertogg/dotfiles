b6f81792a4ffa15c8ef7868f4bcf3cf371064b50
Alberto Grespan <alberto@albertogrespan.com>
2024-03-25 22:29:50 -0400

    Add commands to gitsigns

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -96,7 +96,25 @@ require("lazy").setup({
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup {
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local opts = { buffer = bufnr }
+
+          -- Actions
+          vim.keymap.set("n", "<leader>gb", function() gs.blame_line{full=true} end, opts)
+          vim.keymap.set("n", "<leader>gtb", gs.toggle_current_line_blame, opts)
+          vim.keymap.set("n", "<leader>gd", gs.diffthis, opts)
+          vim.keymap.set("n", "<leader>gD", function() gs.diffthis("~") end, opts)
+          vim.keymap.set("n", "<leader>gtd", gs.toggle_deleted, opts)
+          vim.keymap.set("v", "<leader>gs", function() gs.stage_hunk {vim.fn.line("."), vim.fn.line("v")} end, opts)
+          vim.keymap.set("v", "<leader>gr", function() gs.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end, opts)
+
+          -- Text object
+          vim.keymap.set({"o", "x"}, "ih", ":<C-U>Gitsigns select_hunk<CR>", opts)
+        end
+      }
     end,
   },
 
