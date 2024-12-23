909119b084047a6ef5fe3b8759250f16f42c0ef5
Alberto Grespan <alberto@albertogrespan.com>
2024-12-23 17:20:50 -0400

    Add copilot chat nvim
    This is a work in progress to use copilot in nvim.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -328,6 +328,48 @@ require("lazy").setup({
       lspconfig.ts_ls.setup {}
     end,
   },
+
+  -- CopilotChat.vim with Copilot
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    event = "VeryLazy",
+    opts = {
+      -- See Configuration section for options
+      model = "claude-3.5-sonnet",
+      window = {
+        layout = 'float',
+        relative = 'cursor',
+        width = 0.5,
+        height = 0.5,
+      },
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+    keys = {
+      {
+        "<leader>ccq",
+        function()
+          local input = vim.fn.input("Quick Chat: ")
+          if input ~= "" then
+            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+          end
+        end,
+        desc = "CopilotChat - Quick chat",
+      },
+      {
+        "<leader>ccp",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+        end,
+        desc = "CopilotChat - Prompt actions",
+      },
+    },
+  },
 })
 
 --------------------
